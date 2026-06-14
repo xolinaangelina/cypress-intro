71afd8f5fa3102e64d45badceea85a0cdd38d364
xolinaangelina <xolinaangelina15@gmail.com>
2026-06-14 17:08:36 +0300

    feat: add header and footer tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://example.cypress.io",
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    video: false,
-    screenshotOnRunFailure: true,
+    baseUrl: 'https://qauto.forstudy.space',
+    setupNodeEvents(on, config) {},
   },
 });
